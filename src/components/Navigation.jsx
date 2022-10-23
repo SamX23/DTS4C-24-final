c4f267f95905de27ff85fd6cb41308aa2f953873
Sami Kalammallah <skalexsong@gmail.com>
2022-10-23 17:40:41 +0700

    Update : navigation component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { useAuth } from "../context";
+import { useAuth } from "../hooks/context";
+import { FaSearch, FaSignOutAlt, FaRegBookmark, FaUser } from "react-icons/fa";
+import Button from "./Button";
 
 const Navigation = () => {
   const { logout } = useAuth();
@@ -8,26 +10,36 @@ const Navigation = () => {
 
   return (
     <nav className="fixed left-0 top-0 px-4 py-3 bg-slate-700 w-full flex justify-between items-center">
-      <h1>Nonton Movie</h1>
-      <ul className="flex gap-2">
-        <li>
-          <Link to="wishlist">Wishlist</Link>
-        </li>
-        <li>
-          <Link to="profile">profile</Link>
-        </li>
-      </ul>
-      <div className="flex gap-2">
-        <div>
-          Icon Search
-          <input
-            type="text"
-            placeholder="Search Movies"
-            value={searchResult}
-            onChange={(e) => setSearchResult(e.currentTarget.value)}
-          />
-        </div>
-        <button onClick={() => logout()}>Logout</button>
+      <Link to="/">
+        <h1>Nonton Movie</h1>
+      </Link>
+      <div className="flex bg-white rounded-full p-2 text-black items-center">
+        <FaSearch className="mx-2" />
+        <input
+          type="text"
+          placeholder="Search Movies"
+          value={searchResult}
+          onChange={(e) => setSearchResult(e.currentTarget.value)}
+          className="focus:outline-none"
+        />
+      </div>
+      <div className="flex gap-3">
+        <Link to="wishlist">
+          <Button>
+            <FaRegBookmark className="mr-2" />
+            Wishlist
+          </Button>
+        </Link>
+        <Link to="profile">
+          <Button>
+            <FaUser className="mr-2" />
+            Profile
+          </Button>
+        </Link>
+        <Button onClick={() => logout()}>
+          <FaSignOutAlt className="mr-2" />
+          Logout
+        </Button>
       </div>
     </nav>
   );
