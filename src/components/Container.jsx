50a935e943bb1ecd890a002712282e8a8bfee359
Sami Kalammallah <skalexsong@gmail.com>
2022-10-24 11:20:40 +0700

    Update : registration ui

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,7 @@
-const Container = ({ children }) => {
+const Container = ({ children, noContainer = false }) => {
   return (
-    <div className="relative bg-slate-900 min-h-screen p-2 text-center text-white">
-      <div className="container">{children}</div>
+    <div className="relative bg-slate-900 min-h-screen text-center text-white">
+      {noContainer ? children : <div className="container">{children}</div>}
     </div>
   );
 };
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Navigate } from "react-router-dom";
 import Container from "../components/Container";
 import { useAuth } from "../hooks/context";
+import loginCover from "../assets/login-cover.jpg";
 
 const Login = () => {
   const { user, login } = useAuth();
@@ -13,32 +14,48 @@ const Login = () => {
       {user ? (
         <Navigate to="/" />
       ) : (
-        <Container>
-          <div className="bg-slate-300 border rounded m-auto p-3 text-slate-600">
-            <h1 className="text-center">Login</h1>
-            <div action="" className="mt-2 flex flex-col gap-2">
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.currentTarget.value)}
+        <Container noContainer>
+          <div className="flex min-h-screen">
+            <div className="absolute h-full w-full lg:h-auto lg:relative lg:w-1/2">
+              <img
+                src={loginCover}
+                alt="Login Cover"
+                width="900px"
+                height="900px"
+                className="object-cover h-full w-full"
               />
-              <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.currentTarget.value)}
-              />
-              <button
-                className="text-white block w-full text-sm bg-slate-500 rounded p-2 font-bold"
-                onClick={() => login({ email: email, password: password })}
-              >
-                Login
-              </button>
+              {/* https://unsplash.com/photos/AtPWnYNDJnM */}
+            </div>
+            <div className="absolute h-full w-full lg:h-auto lg:relative lg:w-1/2 flex flex-col justify-center">
+              <div className="bg-slate-300 border rounded p-3 text-slate-600 mx-auto w-96">
+                <h1 className="text-center mb-6 uppercase font-bold">Login</h1>
+                <div className="mt-2 flex flex-col gap-2">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                    value={email}
+                    className="rounded p-2 focus:outline-none"
+                    onChange={(e) => setEmail(e.currentTarget.value)}
+                  />
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                    value={password}
+                    className="rounded p-2 focus:outline-none"
+                    onChange={(e) => setPassword(e.currentTarget.value)}
+                  />
+                  <button
+                    className="text-white block w-full text-sm bg-slate-500 rounded p-2 font-bold"
+                    onClick={() => login({ email: email, password: password })}
+                  >
+                    Register/Login
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </Container>
