644ea7c3ba5456ea045b7ba496cf2c4c1e124868
Sami Kalammallah <skalexsong@gmail.com>
2022-10-24 22:58:25 +0700

    Update : conditional login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import Container from "../components/Container";
 import { useAuth } from "../hooks/context";
 import loginCover from "../assets/login-cover.jpg";
+import GoogleButton from "react-google-button";
 
 const Login = () => {
-  const { user, login } = useAuth();
+  const { user, login, register } = useAuth();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [userLogin, setUserLogin] = useState(true);
 
   return (
     <>
@@ -15,20 +17,27 @@ const Login = () => {
         <Navigate to="/" />
       ) : (
         <Container noContainer>
-          <div className="flex min-h-screen">
+          <div className="flex min-h-screen relative">
             <div className="absolute h-full w-full lg:h-auto lg:relative lg:w-1/2">
               <img
                 src={loginCover}
                 alt="Login Cover"
                 width="900px"
                 height="900px"
-                className="object-cover h-full w-full"
+                className="object-cover h-full w-full bg-slate-500"
+                loading="lazy"
               />
-              {/* https://unsplash.com/photos/AtPWnYNDJnM */}
+              <span className="absolute bottom-5 left-0 right-0 text-sm text-slate-500">
+                Image from https://unsplash.com/photos/AtPWnYNDJnM
+              </span>
             </div>
             <div className="absolute h-full w-full lg:h-auto lg:relative lg:w-1/2 flex flex-col justify-center">
               <div className="bg-slate-300 border rounded p-3 text-slate-600 mx-auto w-96">
-                <h1 className="text-center mb-6 uppercase font-bold">Login</h1>
+                <div className="mb-6 border-b-2 pb-2 border-slate-400">
+                  <h1 className="uppercase font-bold">Proudly Present</h1>
+                  <h2>Nonton Movie</h2>
+                </div>
+
                 <div className="mt-2 flex flex-col gap-2">
                   <input
                     type="email"
@@ -50,11 +59,47 @@ const Login = () => {
                   />
                   <button
                     className="text-white block w-full text-sm bg-slate-500 rounded p-2 font-bold"
-                    onClick={() => login({ email: email, password: password })}
+                    onClick={() => {
+                      userLogin
+                        ? login({ email, password })
+                        : register({ email, password });
+                    }}
                   >
-                    Register/Login
+                    {userLogin ? "Login" : "Register"}
                   </button>
+
+                  {userLogin ? (
+                    <span>
+                      Don't have account ?
+                      <button
+                        className="underline ml-2"
+                        onClick={() => setUserLogin(false)}
+                      >
+                        Register
+                      </button>
+                    </span>
+                  ) : (
+                    <span>
+                      Already have an account ?
+                      <button
+                        className="underline ml-2"
+                        onClick={() => setUserLogin(true)}
+                      >
+                        Login
+                      </button>
+                    </span>
+                  )}
                 </div>
+
+                <span className="my-4 block">Or</span>
+
+                <GoogleButton
+                  type="dark"
+                  className="mx-auto"
+                  onClick={() => {
+                    console.log("Google button clicked");
+                  }}
+                />
               </div>
             </div>
           </div>
